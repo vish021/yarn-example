2208ff3e4bf225d681c6c2f6e6635bbf108628a8
vishal021 <vishal.lambton@gmail.com>
2018-08-02 17:15:09 -0400

    yarn commands and use

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 /**
  * when dependecies version get changed and it's installed on Heroku or AWS, it always installs the latest. Yarn.lock makes sure we use the same version of dependencies everyhwere and hence
  * You should not .gitignore yarn.lock
+ * Yarn has local caching. when  we install express 4.1.6 and next time if we run the same command, it grabd ot from local cache (Offline mode)
+ * It might take a minute for NPM where Yarn takes 30 seconds (Really fast eh!)
+ * yarn run <script_name> to run the script in package.json
  * 
- * 
+ *  
  */
 const express = require('express');
 const port = process.env.PORT || 3000;
